3fa847eb9037be25ca65be6159149d113b2064d5
Adam E <EmersonEXE@pm.me>
2022-12-18 17:13:01 -0600

    fix escaped quotes in literals on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -76,31 +76,32 @@ export default function Explore( ) {
             })}>
                 <Title order={2}>What is Auragraph?</Title>
                 <Text size={'lg'}>
-                    Auragraph is a web application for Spotify users designed by a music enthusiast, for music enthusiasts. We aim to help users visualize the relationships between their favorite artists by generating colorful, interactive euler diagrams that we call, you guessed it, auragraphs.
+                    {`Auragraph is a web application for Spotify users designed by a music enthusiast, for music enthusiasts. We aim to help users visualize the relationships between their favorite artists by generating colorful, interactive euler diagrams that we call, you guessed it, auragraphs.`}
                     <br/> <br/>
-                    Spotify recognizes <em><strong>over 5000</strong></em> unique genres of music. That's a lot to wade through! <br/><br/>
-                    We hope that auragraph will enable people to better understand musical taxonomy while enabling the exploration and discovery of new sounds.
+                    {`Spotify recognizes`} <em><strong> {`over 5000`}</strong></em> {`unique genres of music. That's a lot to wade through!`}
+                    <br/><br/>
+                    {`We hope that auragraph will enable people to better understand musical taxonomy while enabling the exploration and discovery of new sounds.`}
                 </Text>
                 <Divider my={'md'}/>
                 <Title order={2}>How Does It Work?</Title>
                 <Text size={'lg'}>
-                Once you have logged in with your Spotify account (thanks Supabase!), your favorite artists are retrieved via the Spotify API. With your favorite artists in hand, we can get to work on generating your very own auragraph.
-                <br/><br/>
-                Spotify assigns each artist any number of genres, but for our purposes we only care about the genres where your favorite artists overlap. If we didn't cull the dataset your graph would be full of disjoint outliers. Lame!
-                <br/><br/>
-                After we have removed any outlying genres, the euler diagram is generated with D3 and venn.js.
-                <br/><br/>
-                Depending on how diverse your listening habits are, your auragraph might be quite dense and difficult to parse, but you can explore the chart by toggling genres with the button above your graph.
-                </Text>
+                    {`Once you have logged in with your Spotify account (thanks Supabase!), your favorite artists are retrieved via the Spotify API. With your favorite artists in hand, we can get to work on generating your very own auragraph.
+                    `}                <br/><br/>
+                    {`Spotify assigns each artist any number of genres, but for our purposes we only care about the genres where your favorite artists overlap. If we didn't cull the dataset your graph would be full of disjoint outliers. Lame!
+                    `}                <br/><br/>
+                    {`After we have removed any outlying genres, the euler diagram is generated with D3 and venn.js.
+                    `}                <br/><br/>
+                    {`Depending on how diverse your listening habits are, your auragraph might be quite dense and difficult to parse, but you can explore the chart by toggling genres with the button above your graph.
+                    `}                </Text>
                 <Divider my={'md'}/>
                 <Title order={2}>Observations On The Graph</Title>
                 <Text size={"lg"}>
                 <ul>
-                    <li>The genres most represented amongst your top artists will be the drawn largest.</li>
-                    <li>Toggle genres off and on to find new relationships in your graph.</li>
-                    <li>Most graphs will have one or two central genres, like "Pop", or "Rap".</li>
-                    <li>If you turn the central genres off, your graph will become disjoint!.</li>
-                    <li>Some genres will be represented as complete subsets of others. "Art Pop" and "Hyper Pop" for example.</li>
+                    <li>{`The genres most represented amongst your top artists will be the drawn largest.`}</li>
+                    <li>{`Toggle genres off and on to find new relationships in your graph.`}</li>
+                    <li>{`Most graphs will have one or two central genres, like "Pop", or "Rap".`}</li>
+                    <li>{`If you turn the central genres off, your graph will become disjoint!.`}</li>
+                    <li>{`Some genres will be represented as complete subsets of others. "Art Pop" and "Hyper Pop" for example.`}</li>
                 </ul>
                 </Text>
 
